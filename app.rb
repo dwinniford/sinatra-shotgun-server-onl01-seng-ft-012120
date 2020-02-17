5812d9c3aa2a50e4bdbb5e7ab02d578312194ccf
Dave Winniford <dwinniford@gmail.com>
2020-02-17 22:59:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! Wuts up my man!!! "
+    "Welcome to your app!!!! Wuts up my man!!! I'm using shotgun! "
   end
 
 end
